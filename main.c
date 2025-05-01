f5bac7c10750c5c65282a750b670cc726b0d65df
J. Julian Bonilla <jose.julian.bonilla786@gmail.com>
2025-05-01 14:19:14 -0500

    Update main.c

--- main.c
+++ main.c
@@ -11,15 +11,15 @@ int main() {
     int opcion, ganador;
 
     do {
-        printf("\nMenú:\n");
+        printf("\nMenu:\n");
         printf("1. Ingresar peleadores\n");
         printf("2. Insertar resultados\n");
         printf("3. Mostrar finalistas\n");
         printf("4. Salir\n");
-        printf("Seleccione una opción: ");
+        printf("Seleccione una opcion: ");
 
         while (scanf("%d", &opcion) != 1 || opcion < 1 || opcion > 4) {
-            printf("Entrada inválida. Intente nuevamente: ");
+            printf("Entrada invalida. Intente nuevamente: ");
             while(getchar() != '\n'); // Limpiar el buffer de entrada
         }
 
@@ -37,9 +37,9 @@ int main() {
                     printf("1. %s\n", peleadores[i * 2]);
                     printf("2. %s\n", peleadores[i * 2 + 1]);
 
-                    printf("Seleccione el número del ganador (1 o 2): ");
+                    printf("Seleccione el numero del ganador (1 o 2): ");
                     while (scanf("%d", &ganador) != 1 || ganador < 1 || ganador > 2) {
-                        printf("Entrada inválida. Ingrese 1 o 2: ");
+                        printf("Entrada invalida. Ingrese 1 o 2: ");
                         while(getchar() != '\n'); // Limpiar el buffer de entrada
                     }
 
@@ -47,16 +47,32 @@ int main() {
 
                     printf("Ingrese los puntos obtenidos por %s: ", finalistas[i]);
                     while (scanf("%d", &puntos[i]) != 1 || puntos[i] < 0) {
-                        printf("Entrada inválida. Ingrese un número válido de puntos: ");
+                        printf("Entrada invalida. Ingrese un numero valido de puntos: ");
                         while(getchar() != '\n'); // Limpiar el buffer de entrada
                     }
                 }
                 break;
 
             case 3:
-                printf("\nFinalistas:\n");
+                int max1 = 0, max2 = 0, p2 = -1, p = -1;
                 for (int i = 0; i < 3; i++) {
-                    printf("%s con %d puntos\n", finalistas[i], puntos[i]);
+                    if (puntos[i] > max1) {
+                        max2 = max1;
+                        p = p2;
+                        max1 = puntos[i];
+                        p2 = i;
+                    } else if (puntos[i] > max2) {
+                        max2 = puntos[i];
+                        p2 = i;
+                    }
+                }
+                
+                printf("\nFinalistas:\n");
+                if (p2 != -1) {
+                    printf("%s con %d puntos\n", finalistas[p2], puntos[p2]);
+                }
+                if (p != -1) {
+                    printf("%s con %d puntos\n", finalistas[p], puntos[p]);
                 }
                 break;
 
